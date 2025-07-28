2e4f0aa08bdcb88e1468aa3eef16179c877175fa
Kendeki <92603580+Kendeki@users.noreply.github.com>
2025-07-28 16:55:11 -0300

    finished Category, creating create_spend_chart

--- budget_app/main.py
+++ budget_app/main.py
@@ -4,10 +4,18 @@ class Category:
         self.ledger: list[dict[str, float | str]] = []
 
     def __str__(self):
-        return f'{("*" * (15 - len(self.name)//2)) + self.name + ("*" * (15 - len(self.name)//2))}'
+        MAX_CHARACTERS = 23
+        title = f"{create_asterisks(self.name)}\n"
+        transactions = ''.join([
+            i['description'][:MAX_CHARACTERS] + # description
+            ' ' * (30 - len(i['description'][:MAX_CHARACTERS]) - len(f"{(i['amount']):.2f}")) + # spaces between description and amout 
+            f"{(i['amount']):.2f}\n" # amount
+            for i in self.ledger
+                        ])
+        return title + transactions + f'Total: {self.get_balance()}'
 
     def check_funds(self, amount: float) -> bool:
-        return sum([i['amount'] for i in self.ledger]) > amount
+        return sum([i['amount'] for i in self.ledger]) >= amount
 
     def deposit(self, amount: float, description: str = '') -> None:
         self.ledger.append({'amount': amount, 'description': description})
@@ -23,14 +31,53 @@ class Category:
     
     def transfer(self, amount: float, category: 'Category') -> bool:
         if self.check_funds(amount):
-            self.withdraw(amount, 'Transfer to {category.name}')
-            category.deposit(amount, 'Transfer from {self.name}') 
+            self.withdraw(amount, f'Transfer to {category.name}')
+            category.deposit(amount, f'Transfer from {self.name}') 
             return True
         return False
     
-#def create_spend_chart(categories):
-#    pass
+def create_spend_chart(categories: list[Category]) -> str:
+    def check_withdraws(category: Category) -> float:
+        return sum(i['amount'] if i['amount'] < 0 else 0 for i in category.ledger)
+    
+    def num_circles(withdraw: list[float]) -> list[int]:
+        return [int(round(i/sum(withdraw)*100, -1)/10) + 1 for i in withdraw]
+    
+    withdraws_percentage = num_circles(list(map(check_withdraws, categories)))
+    print(withdraws_percentage)
+    
+    title = 'Percentage spent by category\n'
+
+    return ''
+
+def create_asterisks(string: str) -> str:
+    MAX_CHARACTERS = 30
+    pos = True
+
+    while len(string) < MAX_CHARACTERS:
+        if pos:
+            string = '*' + string
+        else:
+            string = string + '*'
+        pos = not pos
+
+    return string
+
+# test case
+
+food = Category('Food')
+food.deposit(1000, 'deposit')
+food.withdraw(240.15, 'groceries')
+food.withdraw(100.89, 'restaurant and more food for dessert')
+clothing = Category('Clothing')
+food.transfer(500, clothing)
+clothing.withdraw(240.15, 'groceries')
 
 a = Category('a')
-a.deposit(1222, 'oi')
-print(a)
+a.deposit(1223, 'skibidi')
+a.withdraw(100, 'groceries')
+a.withdraw(152.89, 'teste')
+b = Category('b')
+a.transfer(500, b)
+b.withdraw(300, 'groceries')
+create_spend_chart([food,clothing,a,b])
