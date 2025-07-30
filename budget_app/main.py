bfa915b5f9afbd983d81289c25ad8614b7427760
Kendeki <92603580+Kendeki@users.noreply.github.com>
2025-07-30 00:50:36 -0300

    pequena modificacao

--- budget_app/main.py
+++ budget_app/main.py
@@ -4,15 +4,17 @@ class Category:
         self.ledger: list[dict[str, float | str]] = []
 
     def __str__(self):
-        MAX_CHARACTERS = 23
-        title = f"{create_asterisks(self.name)}\n"
+        MAX_CHARACTERS_PER_LINE = 30
+        MAX_CHARACTERS_PER_DESCRIPTION = 23
+        title = f"{self.name:*^{MAX_CHARACTERS_PER_LINE}}\n"
         transactions = ''.join([
-            i['description'][:MAX_CHARACTERS] + # description
-            ' ' * (30 - len(i['description'][:MAX_CHARACTERS]) - len(f"{(i['amount']):.2f}")) + # spaces between description and amout 
+            i['description'][:MAX_CHARACTERS_PER_DESCRIPTION] + # description
+            ' ' * (MAX_CHARACTERS_PER_LINE - len(i['description'][:MAX_CHARACTERS_PER_DESCRIPTION]) - len(f"{(i['amount']):.2f}")) + # spaces between description and amout 
             f"{(i['amount']):.2f}\n" # amount
             for i in self.ledger
                         ])
-        return title + transactions + f'Total: {self.get_balance()}'
+        
+        return title + transactions + f'Total: {self.get_balance():.2f}'
 
     def check_funds(self, amount: float) -> bool:
         return sum([i['amount'] for i in self.ledger]) >= amount
@@ -49,20 +51,7 @@ def create_spend_chart(categories: list[Category]) -> str:
     title = 'Percentage spent by category\n'
 
     return ''
-
-def create_asterisks(string: str) -> str:
-    MAX_CHARACTERS = 30
-    pos = True
-
-    while len(string) < MAX_CHARACTERS:
-        if pos:
-            string = '*' + string
-        else:
-            string = string + '*'
-        pos = not pos
-
-    return string
-
+    
 # test case
 
 food = Category('Food')
@@ -72,7 +61,7 @@ food.withdraw(100.89, 'restaurant and more food for dessert')
 clothing = Category('Clothing')
 food.transfer(500, clothing)
 clothing.withdraw(240.15, 'groceries')
-
+print(food)
 a = Category('a')
 a.deposit(1223, 'skibidi')
 a.withdraw(100, 'groceries')
